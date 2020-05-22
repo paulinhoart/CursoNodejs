c228fb1cf7fbe67df135061cbcf0b4b78a71da85
Paulo Roberto <pauloroberto@MacBook-Pro-de-Paulo.local>
2020-05-21 22:19:49 -0300

    atulizando strategy

--- 07-multi-db/README.md
+++ 07-multi-db/README.md
@@ -20,4 +20,10 @@ docker run --name mongodb -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=admin -e 
 //client mongodb
 docker run --name mongoclient -p 3000:3000 --link mongodb:mongodb -d mongoclient/mongoclient
 
+
 docker exec -it mongodb mongo --host localhost -u admin -p admin --authenticationDatabase admin --eval "db.getSiblingDB('herois').createUser({user: 'paulodasilva', pwd:'admin', roles:[{role: 'readWrite', db: 'herois'}]})"
+
+
+
+----SUBIR AS IMAGENS PARA DOCKER HUB
+docker tag mongodb paulinhoart/mongodb
--- 07-multi-db/src/strategy.js
+++ 07-multi-db/src/strategy.js
@@ -21,3 +21,46 @@ class ICrud {
         throw new NotImplementedException()
     }
 }
+
+class MongoDB extends ICrud {
+    constructor() {
+        super()
+    }
+
+    create(item) {
+        console.log('O item foi salvo no MongoDB')
+    }
+}
+
+class Postgres extends ICrud {
+    constructor() {
+        super()
+    }
+
+    create(item) {
+        console.log('O item foi salvo no Postgres')
+    }
+}
+
+//criar uma classe para trabalhar com contexto de banco de dados
+class ContextStrategy extends ICrud {
+    constructor(strategy) {
+        this._database = strategy
+    }
+
+    create(item) {
+        return this._database.create(item)
+    }
+
+    read(item) {
+        return this._database.read(item)
+    }
+
+    update(id, item) {
+        return this._database.update(id, item)
+    }
+
+    delete(id) {
+        return this._database.delete(id)
+    }
+}
