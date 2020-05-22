d29956e7665b7bebb12b64366f73870343afcb06
Paulo Roberto <pauloroberto@MacBook-Pro-de-Paulo.local>
2020-05-21 21:13:50 -0300

    projeto banco

---
+++ 07-multi-db/src/strategy.js
@@ -0,0 +1,23 @@
+class NotImplementedException extends Error {
+    constructor() {
+        super('Not Implemented Exception')
+    }
+}
+//Aqui todas as assinaturas, para todas as estratégias seguir este contrato, tem que implementare quem náo seguir apresentar erro
+class ICrud {
+    create(item) {
+        throw new NotImplementedException()
+    }
+
+    read(query) {
+        throw new NotImplementedException()
+    }
+
+    update(id, item) {
+        throw new NotImplementedException()
+    }
+
+    delete(id) {
+        throw new NotImplementedException()
+    }
+}
