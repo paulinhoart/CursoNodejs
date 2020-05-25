6601ef9b822ac1eae28559c3c4a98f9285f6d21e
Paulo Roberto <pauloroberto@MacBook-Pro-de-Paulo.local>
2020-05-25 09:38:24 -0300

    atualizando

--- 08-multi-db-postgres/src/tests/postgresStrategy.test.js
+++ 08-multi-db-postgres/src/tests/postgresStrategy.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
-const Postgres = require('./../db/strategies/postgres')
-const Context = require('./../db/strategies/base/contextStrategy')
+const Postgres = require('../db/strategies/postgres')
+const Context = require('../db/strategies/base/contextStrategy')
 
 const context = new Context(new Postgres())
 
